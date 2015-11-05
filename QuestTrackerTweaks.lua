7ed4c6aa00c647b9415fc4d543f05788cb8d0c04
Ilazki <ws.ilazki@gmail.com>
2015-11-04 22:52:11 -0500

    Initial commit.

---
+++ QuestTrackerTweaks.lua
@@ -0,0 +1,128 @@
+-----------------------------------------------------------------------------------------------
+-- Client Lua Script for QuestTrackerTweaks
+-- Copyright (c) Ilazki. All rights reserved
+-----------------------------------------------------------------------------------------------
+
+require "Window"
+
+-----------------------------------------------------------------------------------------------
+-- QuestTrackerTweaks Module Definition
+-----------------------------------------------------------------------------------------------
+local QuestTrackerTweaks = {}
+
+-----------------------------------------------------------------------------------------------
+-- Init
+-----------------------------------------------------------------------------------------------
+function QuestTrackerTweaks:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+
+    -- initialize variables here
+
+    return o
+end
+
+function QuestTrackerTweaks:Init()
+	local bHasConfigureFunction = false
+	local strConfigureButtonText = ""
+	local tDependencies = {
+		"QuestTracker"
+	}
+    Apollo.RegisterAddon(self, bHasConfigureFunction, strConfigureButtonText, tDependencies)
+end
+
+
+-----------------------------------------------------------------------------------------------
+-- OnLoad & OnDocLoaded
+-----------------------------------------------------------------------------------------------
+function QuestTrackerTweaks:OnLoad()
+    -- load our form file
+	self.xmlDoc = XmlDoc.CreateFromFile("QuestTrackerTweaks.xml")
+	self.xmlDoc:RegisterCallback("OnDocumentReady", self)
+end
+
+function QuestTrackerTweaks:OnDocumentReady()
+	if self.xmlDoc ~= nil and self.xmlDoc:IsLoaded() then
+	    self.wndMain = Apollo.LoadForm(self.xmlDoc, "QuestTrackerRightClick", nil, self)
+		if self.wndMain == nil then
+			Apollo.AddAddonErrorText(self, "Could not load the main window for some reason.")
+			return
+		end
+
+	    self.wndMain:Show(false, true)
+
+		--- Replace QuestTracker's right click menu
+    self.ReplaceQTRightClick()
+	end
+end
+
+-----------------------------------------------------------------------------------------------
+-- Other Functions
+-----------------------------------------------------------------------------------------------
+
+--- This function adds an Abandon handler to QuestTracker, loads a modified rightclick form,
+--- and overrides QuestTracker:ShowRightClick() to use the new form and handler.
+function QuestTrackerTweaks.ReplaceQTRightClick()
+	QuestTracker = Apollo.GetAddon("QuestTracker")
+	if not QuestTracker then return nil end
+
+	--- Add an "Abandon" handler method to the QuestTracker
+	function QuestTracker:OnRightClickAbandonBtn( wndHandler, wndControl)
+		local queQuest = wndHandler:GetData()
+    --- Unpin before abandoning to avoid some pinned quests crashing the tracker.
+    self.tPinned.tQuests[queQuest:GetId()] = nil
+
+	 	queQuest:SetActiveQuest(false)
+		queQuest:Abandon()
+		self:OnQuestTrackerRightClickClose()
+
+	end
+	--- Load the replacement Form and set a couple variables the normal tracker uses internally
+	--- since they aren't accesible here.
+	local rightClickReplace = Apollo.GetAddon("QuestTrackerTweaks").xmlDoc
+	local knXCursorOffset = 10
+	local knYCursorOffset = 25
+
+	--- Mostly duplicated from the Carbine quest tracker.
+  --- Changed or added lines are commented with "--- Ilazki" so I know what to edit whenever
+  --- I need to update the function with changed Carbine code.
+	function QuestTracker:ShowQuestRightClick(queQuest)
+		self:OnQuestTrackerRightClickClose()
+
+		self.wndQuestRightClick = Apollo.LoadForm(rightClickReplace, "QuestTrackerRightClick", nil, self)	--- Ilazki
+		self.wndQuestRightClick:FindChild("RightClickOpenLogBtn"):SetData(queQuest)
+		self.wndQuestRightClick:FindChild("RightClickShareQuestBtn"):SetData(queQuest)
+		self.wndQuestRightClick:FindChild("RightClickLinkToChatBtn"):SetData(queQuest)
+		self.wndQuestRightClick:FindChild("RightClickMaxMinBtn"):SetData(queQuest)
+		self.wndQuestRightClick:FindChild("RightClickPinUnpinBtn"):SetData(queQuest)
+		self.wndQuestRightClick:FindChild("RightClickHideBtn"):SetData(queQuest)
+		self.wndQuestRightClick:FindChild("RightClickAbandonBtn"):SetData(queQuest)							--- Ilakzi
+
+		self.wndQuestRightClick:FindChild("RightClickShareQuestBtn"):Enable(queQuest:CanShare())
+
+		local nQuestId = queQuest:GetId()
+		local bAlreadyMinimized = nQuestId and self.tMinimized.tQuests[nQuestId]
+		self.wndQuestRightClick:FindChild("RightClickMaxMinBtn"):SetText(bAlreadyMinimized and Apollo.GetString("QuestTracker_Expand") or Apollo.GetString("QuestTracker_Minimize"))
+		self.wndQuestRightClick:FindChild("RightClickMaxMinBtn"):Enable(queQuest and queQuest:GetState() ~= Quest.QuestState_Botched)
+
+		local bAlreadyPinned = nQuestId and self.tPinned.tQuests[nQuestId]
+		self.wndQuestRightClick:FindChild("RightClickPinUnpinBtn"):SetText(bAlreadyPinned and Apollo.GetString("QuestTracker_Unpin") or Apollo.GetString("QuestTracker_Pin"))
+
+		local tCursor = Apollo.GetMouse()
+		local nWidth = self.wndQuestRightClick:GetWidth()
+		self.wndQuestRightClick:Move(tCursor.x - nWidth + knXCursorOffset, tCursor.y - knYCursorOffset, nWidth, self.wndQuestRightClick:GetHeight())
+	end
+end
+
+-----------------------------------------------------------------------------------------------
+-- QuestTrackerTweaksForm Functions
+-----------------------------------------------------------------------------------------------
+local QuestTrackerTweaksInst = QuestTrackerTweaks:new()
+QuestTrackerTweaksInst:Init()
+
+-----------------------------------------------------------------------------------------------
+-- QuestTrackerTweaks Instance
+-----------------------------------------------------------------------------------------------
+local QuestTrackerTweaksInst = QuestTrackerTweaks:new()
+QuestTrackerTweaksInst:Init()
