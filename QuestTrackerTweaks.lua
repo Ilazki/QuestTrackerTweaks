ca98140fe7a9800973345533fb02e91bc022fff2
Ilazki <ws.ilazki@gmail.com>
2015-11-12 01:10:56 -0500

    Added tweak: new objective auto-select behaviour and updated README

--- QuestTrackerTweaks.lua
+++ QuestTrackerTweaks.lua
@@ -6,9 +6,16 @@
 require "Window"
 
 -----------------------------------------------------------------------------------------------
--- QuestTrackerTweaks Module Definition
+-- QuestTrackerTweaks Module Definition, Constants, & Forward Declarations
 -----------------------------------------------------------------------------------------------
 local QuestTrackerTweaks = {}
+
+--- Forward declarations
+local returnQuestTracker
+
+--- Constants used internally by QuestTracker that must be recreated here for overriden bits.
+local knXCursorOffset = 10
+local knYCursorOffset = 25
 
 -----------------------------------------------------------------------------------------------
 -- Init
@@ -40,7 +47,7 @@ end
 -- OnLoad & OnDocLoaded
 -----------------------------------------------------------------------------------------------
 function QuestTrackerTweaks:OnLoad()
-    -- load our form file
+    -- load QTT's form file
 	self.xmlDoc = XmlDoc.CreateFromFile("QuestTrackerTweaks.xml")
 	self.xmlDoc:RegisterCallback("OnDocumentReady", self)
 end
@@ -56,7 +63,15 @@ function QuestTrackerTweaks:OnDocumentReady()
 	    self.wndMain:Show(false, true)
 
 		--- Replace QuestTracker's right click menu
-    self.ReplaceQTRightClick()
+		local QT = returnQuestTracker()
+		if QT then
+		   self:ReplaceQTRightClick(QT)
+		   self:ReplaceHintArrowHelper(QT)
+		   -- Something to experiment with to tweak performance/responsiveness tradeoff.
+		   -- Not ready to use it yet.
+		   -- QT.timerRealTimeUpdate = ApolloTimer.Create(10.0, true, "OnRealTimeUpdateTimer", self)
+		   -- QT.timerRealTimeUpdate:Stop()
+		end
 	end
 end
 
@@ -64,7 +79,7 @@ end
 function QuestTrackerTweaks:OnDependencyError(dep, err)
 --- QuestTracker is only hard dependnecy; don't freak out over SQT or my copy of CRB's tracker
    if dep ~= "QuestTracker" then return true end
---   return true
+   return true
    
 end
 
@@ -74,16 +89,79 @@ end
 -- Other Functions
 -----------------------------------------------------------------------------------------------
 
+--- Returns a QuestTracker object to mangle.  Avoids adding multi-addon checks each function.
+returnQuestTracker = function()
+   return Apollo.GetAddon("QuestTracker") or Apollo.GetAddon("QuestTracker_CRB")
+end
+
+----------------------------
+-- ReplaceHintArrowHelper
+----------------------------
+
+--- Overrides the Hint Arrow Helper to add more user-friendly behaviour.  In this version, if
+--- the selected objective is completed, it selects the first uncompleted objective in the
+--- same quest.  This makes more sense than the Carbine version selecting the overarching
+--- quest because it makes the interact key more useful for quest pointing.
+--- Acts the same as Carbine's version in any other case.
+function QuestTrackerTweaks:ReplaceHintArrowHelper(QuestTracker)
+   --- The new QuestTracker function.
+   function QuestTracker:HelperSelectInteractHintArrowObject(oCur, wndBtn)
+
+	  --- Takes one quest argument and returns two:
+	  ---  1: the QuestTracker's objWnd UI element for the quest's first uncompleted Objective
+	  ---  2: the index vale of the Objective.
+
+	  --- local function to avoid extra-deep nesting.  QT code is already bad enough about it
+	  --- without me contributing to the mess in here.
+	  local function getObjectiveWnd(currentQuest)
+		 local questID   = currentQuest:GetId()
+		 local objCount  = currentQuest:GetObjectiveCount()
+		 for i = 0, objCount - 1 do
+			local objKey = questID .. "O" .. i   --- QT caches objective info in a table using
+			                                     --- Quest ID + Objective Index
+			                                     --- Format: qqqqOi, with uppercase o as a
+			                                     --- separator between quest ID and index.
+			local objWnd = self.tObjectiveWndCache[objKey]
+			if objWnd and currentQuest:GetObjectiveCompleted(i) == 0 then return objWnd end
+		 end
+	  end
+
+	  --- Carbine code, umodified
+	  local oInteractObject = GameLib.GetInteractHintArrowObject()
+	  if not oInteractObject or (oInteractObject and oInteractObject.eHintArrowType == GameLib.CodeEnumHintType.None) then
+		 return
+	  end
+
+	  local bIsInteractHintArrowObject = oInteractObject.objTarget and oInteractObject.objTarget == oCur
+	  if bIsInteractHintArrowObject and not wndBtn:IsChecked() then
+
+		 --- Try to get the objWnd object for an uncompleted sibling Objective to the current quest.
+		 --- If successful, selects that Objective as the new quest object.  
+		 local objWnd, objIndex = getObjectiveWnd(oCur)
+		 if objWnd then
+			objWnd.wndObjective:FindChild("QuestObjectiveBtn"):SetCheck(true)
+			GameLib.SetInteractHintArrowObject(oCur, objIndex)
+		 else
+			--- Otherwise, revert to Carbine's behaviour.
+			wndBtn:SetCheck(true)
+		 end
+	 
+	  end
+   end
+   
+end
+
+-------------------------
+-- ReplaceQTRightClick
+-------------------------
+
 --- This function adds an Abandon handler to QuestTracker, loads a modified rightclick form,
 --- and overrides QuestTracker:ShowRightClick() to use the new form and handler.
-function QuestTrackerTweaks.ReplaceQTRightClick()
-   QuestTracker = Apollo.GetAddon("QuestTracker") or Apollo.GetAddon("QuestTracker_CRB")
-   if not QuestTracker then return nil end
-
-	--- Add an "Abandon" handler method to the QuestTracker
+function QuestTrackerTweaks:ReplaceQTRightClick(QuestTracker)
+   --- Add an "Abandon" handler method to the QuestTracker that gets called by the new popup
 	function QuestTracker:OnRightClickAbandonBtn( wndHandler, wndControl)
 		local queQuest = wndHandler:GetData()
-    --- Unpin before abandoning to avoid some pinned quests crashing the tracker.
+    --- Unpin quest before abandoning to avoid some pinned quests crashing the tracker.
     self.tPinned.tQuests[queQuest:GetId()] = nil
 
 	 	queQuest:SetActiveQuest(false)
@@ -94,12 +172,10 @@ function QuestTrackerTweaks.ReplaceQTRightClick()
 	--- Load the replacement Form and set a couple variables the normal tracker uses internally
 	--- since they aren't accesible here.
 	local rightClickReplace = Apollo.GetAddon("QuestTrackerTweaks").xmlDoc
-	local knXCursorOffset = 10
-	local knYCursorOffset = 25
 
 	--- Mostly duplicated from the Carbine quest tracker.
-  --- Changed or added lines are commented with "--- Ilazki" so I know what to edit whenever
-  --- I need to update the function with changed Carbine code.
+	--- Changed or added lines are commented with "--- Ilazki" so I know what to edit whenever
+	--- I need to update the function with changed Carbine code.
 	function QuestTracker:ShowQuestRightClick(queQuest)
 		self:OnQuestTrackerRightClickClose()
 
--- Readme.md
+++ Readme.md
@@ -1,7 +1,21 @@
-## Quest Tracker Tweaks
+## About Quest Tracker Tweaks
 
-This addon is going to be the home for any changes or bugfixes I make to the default Carbine QuestTracker for Wildsar.
+This addon is going to be the home for any changes or bugfixes I make to the default Carbine QuestTracker for Wildstar.  It works by overloading parts of the default Quest Tracker with new code to add features or fix problems.  The goal is to improve the quality of Carbine's tracker without diverging from its design.  Extreme changes will be avoided here, though I may try making a 100% new tracker later as a side project.
 
-Currently, the only tweak is to add an "Abandon" button to the tracker's rightclick popup menu, underneath the "Hide" button.
+So far it doesn't seem to add any major bugs to the tracker that I can find, but it's hard to be sure because the Carbine tracker is already pretty buggy.  Bug reports and feature requests welcome.
 
-So far it doesn't seem to add any major bugs to the tracker that I can find, but it's hard to be sure because the Carbine tracker is already pretty buggy.
+This addon *may* work with Simple Quest Tracker, but consider it untested and unsupported.  YMMV.
+
+## Features
+
+* Add an "Abandon" menu entry to the tracker's right-click popup menu, underneath the "Hide" button.  No need to use the quest log to drop quests any more.
+* Changed the behaviour of objective selection when completing a selected objective.  When the selected objective is completed, the tracker now attempts to select the first uncompleted objective within the same quest.  If this fails it reverts to the default tracker behaviour of selecting the quest itself.
+
+
+## Future Ideas
+
+* Extra checks for the quest filtering.  It mostly works as-is but does some weird things when "indoors"
+* Optional auto-pinning of nearby quests
+* Improve the tracker's information density somehow.  There's a lot of extra space that could be reduced, and also the possibility of doing tricks like auto-collapsing quests outside of a configurable range.
+* Try to improve performance.
+* Fix some of Carbine's tracker bugs, if possible.
